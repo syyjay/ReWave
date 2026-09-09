5fc5f028100f42aaca14440e4dc2208ccc665adf
subo <syyjay@sina.cn>
2026-09-09 11:43:41 +0800

    Initial Commit

---
+++ ReWave/ReWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReWaveApp.swift
+//  ReWave
+//
+//  Created by syy on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ReWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
